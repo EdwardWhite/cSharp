a3aca6833730a27951c96369fc8f3f0b418451c5
Roshan <Roshan@ROSHANSSDPC850>
2016-08-31 21:07:29 +0100

    Addedd File Selector Addedd Filter

---
+++ ConsoleApplication1/ConsoleApplication1/Filter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+  
+    public class Filter
+    {
+       
+        private String filter;
+        private String CleanedFilter;
+
+        public Filter(String n)
+        {
+            filter = n;
+        }
+
+        public String Cleaned()
+        {
+            cleaning();
+            return CleanedFilter;
+        }
+
+        private void cleaning()
+        {
+            var sb = new StringBuilder();
+
+            foreach (char c in filter)
+            {
+                if (char.IsLetterOrDigit(c))
+                    sb.Append(c);
+            }
+            
+            CleanedFilter = sb.ToString();
+        }
+
+    }
+
+
+
+   
+}
---
+++ ConsoleApplication1/ConsoleApplication1/fileSelector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+    class fileSelector
+    {
+        private String fileType ="*";
+        private String strfilename ="";
+
+        public fileSelector(String types)
+        {
+          fileType = types;
+         }
+     
+        public String filepath()
+        {
+            return filelocation();
+        }
+
+        public void fileSelectors()
+        {
+            Fileselector();
+        }
+
+
+
+
+
+        private String filelocation()
+        {
+            return strfilename;
+        }
+
+
+        private void Fileselector()
+        {
+           
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "All Files (*.*)|*."+fileType;
+            choofdlog.FilterIndex = 1;
+            choofdlog.Multiselect = true;
+
+            if (choofdlog.ShowDialog() == DialogResult.OK)
+            {
+                string sFileName = choofdlog.FileName;
+                string[] arrAllFiles = choofdlog.FileNames; //used when Multiselect = true         
+                strfilename = choofdlog.InitialDirectory + choofdlog.FileName;
+            }
+        }
+
+
+    }
+}
